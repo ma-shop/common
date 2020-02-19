cd2d63611723902132c7fb93f9fa681416891a56
Tyler Benton <tjbenton21@gmail.com>
2020-02-19 10:01:34 -0500

    @ma-shop/locale
    created a locale manager to make dealing with locale specific stuff easier

---
+++ packages/locale/README.md
@@ -0,0 +1,3 @@
+# @ma-shop/locale
+
+This package was created to make dealing with locales easier and dealing with them in a standard way.
---
+++ packages/locale/test/index.test.mjs
@@ -0,0 +1,26 @@
+import { LocaleManager } from '../src'
+
+
+describe('locale', () => {
+  let locale
+  beforeEach(() => {
+    locale = new LocaleManager()
+  })
+
+  test('parse', () => {
+    const obj = locale.parse('en-US')
+
+    expect(obj).toMatchSnapshot()
+    expect(locale.parse('United States')).toBe(obj)
+    expect(locale.parse('US')).toBe(obj)
+    expect(locale.parse('USA')).toBe(obj)
+    expect(locale.parse('USD')).toBe(obj)
+  })
+
+  describe.each(LocaleManager.marketLocales)('%s', (current) => {
+    test.each(Object.keys(LocaleManager.localeItems[0]))('%s', (key) => {
+      locale.setLocale(current)
+      expect(locale[key]).toMatchSnapshot()
+    })
+  })
+})
