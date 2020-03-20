903afe8bc1068d4d3613baa0291ab9523b3113de
William Lacy <williamlacy@Williams-MacBook-Pro.local>
2020-03-20 16:22:59 -0400

    utils/utils -> haptic folder

--- utils/haptic/src/index.mjs
+++ utils/haptic/src/index.mjs
@@ -2,8 +2,4 @@
 // !!! This file is generated do not modify it manually because it will be overwritten
 // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-export * from './delay'
-export * from './lodash'
-export * from './noop'
-export * from './packages'
-export * from './round'
+export * from './haptic'
