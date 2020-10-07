a411f484b50ed89a9bfc3d21f4d45666e1843669
Tyler Benton <tjbenton21@gmail.com>
2020-10-07 10:08:18 -0400

    noSpacing,  noChildSpacing, noFirstChildSpacing

--- utils/react-native/src/spacing.mjs
+++ utils/react-native/src/spacing.mjs
@@ -1,6 +1,8 @@
-import { ui } from '@ma-shop/utils'
+import { Children, cloneElement, isValidElement } from 'react'
+import { mapProps, ui } from '@ma-shop/utils'
 
 import { em } from './font'
+
 
 /// @name spacing
 /// @description this handles the spacing and returns the appropriate pixel value
@@ -36,4 +38,75 @@ export function spacing (value = 1, _) {
   }
 
   return em(value, base)
+}
+
+
+/// @name noSpacing
+/// @author Tyler Benton
+/// @description
+/// `noSpacing` is used to add or remove marginTop based
+/// on the `noSpacing` prop that's passed in
+/// @example
+/// function Something({ children, style, ...props }) {
+///   return (
+///     <View style={[ noSpacing(props), style ]}>
+///       {children}
+///     </View>
+///   )
+/// }
+export function noSpacing ({ noSpacing: noSpacingProp, styles: extraStyles }, value = spacing()) {
+  const styles = { ...extraStyles }
+
+  if (!styles.marginTop) {
+    styles.marginTop = value
+  }
+  if (noSpacingProp) {
+    styles.marginTop = 0
+  }
+
+  return styles
+}
+
+/// @name noChildSpacing
+/// @author Tyler Benton
+/// @description
+/// This will set the `noSpacing` prop to true on all the children that are passed to it
+/// @example
+/// function Something({ children }) {
+///   return (
+///     <View>
+///       {noChildSpacing(children)}
+///     </View>
+///   )
+/// }
+export function noChildSpacing (children, extraProps = {}) {
+  return mapProps(children, {
+    noSpacing: true,
+    ...extraProps,
+  })
+}
+
+/// @name noFirstChildSpacing
+/// @author Tyler Benton
+/// @description
+/// This will set the `noSpacing` prop to `true` on the first child in the list
+/// @example
+/// function Something({ children }) {
+///   return (
+///     <View>
+///       {noFirstChildSpacing(children)}
+///     </View>
+///   )
+/// }
+export function noFirstChildSpacing (obj, extraProps = {}) {
+  const children = Children.toArray(obj)
+  if (!isValidElement(children[0])) return children
+
+  const firstChild = cloneElement(children[0], {
+    key: children[0]?.key || 'first-child',
+    noSpacing: true,
+    ...extraProps,
+  })
+
+  return [ firstChild, ...children.slice(1) ]
 }
